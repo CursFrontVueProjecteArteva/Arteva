fb74cd0bc0079cc9569cffa72e9ea6fd45aee3f7
Raul Garcia <68115954+rawl0408@users.noreply.github.com>
2022-03-10 13:52:45 +0100

    jueves, hemos comentado algunos campos de la SHOP y los ARTICULOS, tambien hemos creado el archivo data.js y hemos rellenado con datos FAKE, hemos creado Sprint2 y hemos empezado a trabajar en el

---
+++ assets/js/data.js
@@ -0,0 +1,50 @@
+var products = [
+    {
+        id: 1,
+        name: "Anillo",
+        description: "Este anillo es magnificamente bonito",
+        price: 10,
+        type: "Anillos",
+        img: XXX,
+        material: "Plata",
+        size:  {
+            size1: 14,
+            size2: 16,
+            size3: 18,
+        },
+
+    },
+    {
+        id: 2,
+        name: "Pulsera",
+        description: "Esta pulsera te hará volar",
+        price: 15,
+        type: "Pulseras",
+        img: XYX,
+        material: "Acero inoxidable",
+    },
+    {
+        id: 1,
+        name: "Collar",
+        description: "Este collar te ara ahogar",
+        price: 20,
+        type: "Collares",
+        img: XXX,
+        material: "Acero inoxidable",
+        size:  {
+            size1: 42,
+            size2: 45,
+            size3: 50,
+        },
+
+    },
+    {
+        id: 1,
+        name: "Pendiente",
+        description: "Un pendiente que tiene un diente",
+        price: 5,
+        type: "Pendientes",
+        img: XXX,
+        material: "Oro y Plata",
+    },
+]
