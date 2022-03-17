3fa7fd0cd57196923219cc25df7cba4a9d40a584
elimolina <emolinade@uoc.es>
2022-03-17 12:33:05 +0100

    taiga-54

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,14 @@ function addToCart(id) {
     cart.push(products.find((ele) => ele.id === id));
 }
 // console.log(cart);
+
+
+// función remove All from Card
+
+function removeAllFromCart() {
+    cart.length = 0;
+    //console.log(cart);
+}
+
+
+
