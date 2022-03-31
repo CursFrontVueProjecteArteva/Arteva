0fcc997a121e8571bd8abdb52b4ff543a2405b9a
Raul Garcia <68115954+rawl0408@users.noreply.github.com>
2022-03-31 13:52:54 +0200

    intentar

--- assets/js/app.js
+++ assets/js/app.js
@@ -92,3 +92,63 @@ function getProductByCategory(buscado) {
   console.table(productos);
   return productos;
 }
+
+const templateCarrito = document.getElementById('template-carrito').content
+const templateFooter = document.getElementById('template-footer').content
+const fragment = document.createDocumentFragment()
+
+//FUNCION PINTA template-CARRITO
+
+const pintarCarrito = () => {
+  items.innerHTML = ''
+
+  Object.values(carrito).forEach(producto => {
+      templateCarrito.querySelector('th').textContent = producto.id
+      templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+      templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+      templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+      
+      //botones
+      templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+      templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+      const clone = templateCarrito.cloneNode(true)
+      fragment.appendChild(clone)
+  })
+  items.appendChild(fragment)
+
+  pintarFooter()
+}
+
+//FUNCION PINTA template-FOOTER
+
+const pintarFooter = () => {
+  footer.innerHTML = ''
+  
+  if (Object.keys(carrito).length === 0) {
+      footer.innerHTML = `
+      <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
+      `
+      return
+  }
+  
+  // sumar cantidad y sumar totales
+  const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+  const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
+  // console.log(nPrecio)
+
+  templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+  templateFooter.querySelector('span').textContent = nPrecio
+
+  const clone = templateFooter.cloneNode(true)
+  fragment.appendChild(clone)
+
+  footer.appendChild(fragment)
+
+  const boton = document.querySelector('#vaciar-carrito')
+  boton.addEventListener('click', () => {
+      carrito = {}
+      pintarCarrito()
+  })
+
+}
