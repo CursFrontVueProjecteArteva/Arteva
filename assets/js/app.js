267e68495f1299e05f9d61eeb3eecef80c4059b1
Raul Garcia <68115954+rawl0408@users.noreply.github.com>
2022-04-05 13:57:15 +0200

    ok martes

--- assets/js/app.js
+++ assets/js/app.js
@@ -153,6 +153,23 @@ const pintarCarrito = () => {
 
   pintarFooter()
 }
+// pintar carrito deberia quedar asi:
+
+// const pintarCarrito = () => {
+//   const items = document.getElementById('items');
+//   items.innerHTML ='';
+
+//   cart.forEach(item => {
+//       templateCarrito.querySelector('th').textContent = item.id
+//       templateCarrito.querySelectorAll('td')[0].textContent = item.name
+//       templateCarrito.querySelectorAll('td')[1].textContent = item.price
+//       templateCarrito.querySelectorAll('td')[2].textContent = item.quantity
+//       templateCarrito.querySelector('span').textContent = item.subtotal
+//       const clone = templateCarrito.cloneNode(true)
+//       fragment.appendChild(clone)
+//   })
+//   items.appendChild(fragment)
+// }
 
 //FUNCION PINTA template-FOOTER
 
