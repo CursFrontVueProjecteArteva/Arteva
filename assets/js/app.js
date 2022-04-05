f4c57b71362a1497d2c4e8e803d1cf3a01500c86
EvaTorGarcia <etorg@uoc.edu>
2022-04-05 13:56:44 +0200

    Canvis sprint3 Eva

--- assets/js/app.js
+++ assets/js/app.js
@@ -92,3 +92,27 @@ function getProductByCategory(buscado) {
   console.table(productos);
   return productos;
 }
+
+// Añadir Eva Funcion Carrito Modal ()
+
+function printCartModal() {
+    const items = document.getElementById('itemsModal');
+    items.innerHTML ='';
+
+    cart.forEach(item => {
+        templateCarrito.querySelector('th').textContent = item.id
+        templateCarrito.querySelectorAll('td')[0].textContent = item.name
+        templateCarrito.querySelectorAll('td')[1].textContent = item.price
+        templateCarrito.querySelectorAll('td')[2].textContent = item.quantity
+        templateCarrito.querySelector('span').textContent = item.subtotal
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    itemsModal.appendChild(fragment)
+
+    // printem el total
+
+    const footer = document.querySelector('#totalCarrito');
+    footer.textContent = "EUR" + calculateTotal();
+    //footer.appendChild();
+}
